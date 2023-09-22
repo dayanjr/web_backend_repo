2e8e7c25fd9ec0d26067c1eb48af9f80d43c0ec5
dayanjr <dayanfrazao@byui.edu>
2023-09-22 14:07:03 -0600

    up

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
  *************************/
 const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
+
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
