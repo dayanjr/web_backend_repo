a7440b72b962e84d680c0177eef3bf143f369a67
dayanjr <dayanfrazao@byui.edu>
2023-10-11 17:46:55 -0600

    up

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 /* ***********************
  * Require Statements
  *************************/
-const inventoryRoute = require("C:/Users/h_linford0410/Dropbox/PC/Documents/GitHub/web_backend_repo/routes/inventoryRoute")
+const inventoryRoute = require("./routes/inventoryRoute")
 const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
