c8ebc828c827b5fb3adc643681384fb87329efb1
dayanjr <dayanfrazao@byui.edu>
2023-12-05 14:10:15 -0700

    up

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -9,14 +9,14 @@ const NewInvValidate = require("../utilities/newinv_validation")
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 router.get("/detail/:inv_id", utilities.handleErrors(invController.buildByInventoryId));
 router.get("/", utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(invController.buildManagement));
-router.get("/newinv", utilities.handleErrors(invController.buildNewInv))
+router.get("/newinv",utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(invController.buildNewInv))
 router.post("/newinv",NewInvValidate.registationRules(),NewInvValidate.checkRegData,utilities.handleErrors(invController.registerNewInv))
-router.get("/newclass", utilities.handleErrors(invController.buildNewClass))
+router.get("/newclass",utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(invController.buildNewClass))
 router.post("/newclass",NewClassValidate.registationRules(),NewClassValidate.checkRegData,utilities.handleErrors(invController.registerNewClass))
-router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON))
-router.get("/edit/:inv_id", utilities.handleErrors(invController.editInventoryView))
+router.get("/getInventory/:classification_id", utilities.checkLogin, utilities.checkAccountType,utilities.handleErrors(invController.getInventoryJSON))
+router.get("/edit/:inv_id",utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(invController.editInventoryView))
 router.post("/update/", invController.updateInventory)
-router.get("/delete/:inv_id", utilities.handleErrors(invController.deleteView))
+router.get("/delete/:inv_id", utilities.checkLogin, utilities.checkAccountType,utilities.handleErrors(invController.deleteView))
 router.post("/delete", utilities.handleErrors(invController.deleteItem))
 
 
